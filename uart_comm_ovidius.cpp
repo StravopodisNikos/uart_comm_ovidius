1c7f56cdc406238e27617a4f2d3d3a1718fd5ec7
SyrosRoboticsResearch1 <stravnik.rob@gmail.com>
2023-06-18 15:31:02 +0300

    added ::, bug fixed

--- uart_comm_ovidius.cpp
+++ uart_comm_ovidius.cpp
@@ -11,38 +11,28 @@
     }
 
     template<typename T>
-    void split_32bits_to_bytes(T val, uint8_t *byte_array) {
+    void uart_comm_ovidius::split_32bits_to_bytes(T val, uint8_t *byte_array) {
         uint8_t *ptr = reinterpret_cast<uint8_t*>(&val);
         for (int i = 0; i < BIT32_ARRAY_SIZE; i++) {
             byte_array[i] = ptr[i];
         }
     }
-    template void split_32bits_to_bytes<float>(float val, uint8_t *byte_array);
-    template void split_32bits_to_bytes<long>(long val, uint8_t *byte_array);
+    template void uart_comm_ovidius::split_32bits_to_bytes<float>(float val, uint8_t *byte_array);
+    template void uart_comm_ovidius::split_32bits_to_bytes<long>(long val, uint8_t *byte_array);
 
     template<typename R>
-    void merge_bytes_to_32bits(R & val, uint8_t *byte_array) {
+    void uart_comm_ovidius::merge_bytes_to_32bits(R & val, uint8_t *byte_array) {
         uint8_t *ptr = reinterpret_cast<uint8_t*>(&val);
         for (int i = 0; i < BIT32_ARRAY_SIZE; i++) {
             ptr[i] = byte_array[i];
         }
         return;
     }
-    template void merge_bytes_to_32bits<float&>(float&, uint8_t *byte_array);
-    template void merge_bytes_to_32bits<long&>(long&, uint8_t *byte_array);
-
-    void print4Bytes(Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array) {
-        debug_serial.println("8");
-        for (int i = 0; i < BIT32_ARRAY_SIZE; i++){
-            while(!comm_serial.available());
-            *(Byte4Array + i) = comm_serial.read();
-            debug_serial.print("Received Byte["); debug_serial.print(i); debug_serial.print("]:");
-            debug_serial.println(Byte4Array[i],BIN);
-        }
-    }
+    template void uart_comm_ovidius::merge_bytes_to_32bits<float&>(float&, uint8_t *byte_array);
+    template void uart_comm_ovidius::merge_bytes_to_32bits<long&>(long&, uint8_t *byte_array);
 
     template<typename T>
-    void sendBack4bytes(T val, Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array) {
+    void uart_comm_ovidius::sendBack4bytes(T val, Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array) {
         debug_serial.println("8");
         // Break val to 4 bytes
         split_32bits_to_bytes(val, Byte4Array);
@@ -54,6 +44,29 @@
         }
         return;
     }
-    template void sendBack4bytes<float>(float, Stream&, Stream&, uint8_t *);
-    template void sendBack4bytes<long>(long, Stream&, Stream&, uint8_t * );
+    template void uart_comm_ovidius::sendBack4bytes<float>(float, Stream&, Stream&, uint8_t *);
+    template void uart_comm_ovidius::sendBack4bytes<long>(long, Stream&, Stream&, uint8_t * );
+
+    template<typename S>
+    void uart_comm_ovidius::sendBack4bytes_num(S val, Stream& comm_serial, uint8_t * Byte4Array) {
+        // Break val to 4 bytes
+        split_32bits_to_bytes(val, Byte4Array);
+        // Send the 4 bytes
+        for (int i = 0; i < BIT32_ARRAY_SIZE; i++){
+            comm_serial.write(Byte4Array[i]);
+        }
+        return;
+    }
+    template void uart_comm_ovidius::sendBack4bytes_num<float>(float, Stream&, uint8_t *);
+    template void uart_comm_ovidius::sendBack4bytes_num<long>(long, Stream&, uint8_t * );
+
+    void uart_comm_ovidius::print4Bytes(Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array) {
+        debug_serial.println("8");
+        for (int i = 0; i < BIT32_ARRAY_SIZE; i++){
+            while(!comm_serial.available());
+            *(Byte4Array + i) = comm_serial.read();
+            debug_serial.print("Received Byte["); debug_serial.print(i); debug_serial.print("]:");
+            debug_serial.println(Byte4Array[i],BIN);
+        }
+    }
 
--- uart_comm_ovidius.h
+++ uart_comm_ovidius.h
@@ -12,9 +12,13 @@ namespace uart_comm_ns {
     class uart_comm_ovidius
     {
     private:
-        /* data */
+        float _data_received_f;
+        long _data_received_l;
+         
     public:
-        uart_comm_ovidius(/* args */);
+        uint8_t bit32_array[BIT32_ARRAY_SIZE];
+
+        uart_comm_ovidius();
         ~uart_comm_ovidius();
 
         template<typename T>
@@ -25,16 +29,14 @@ namespace uart_comm_ns {
 
         template<typename T>
         void sendBack4bytes(T val, Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array);
+    
+        template<typename S>
+        void sendBack4bytes_num(S val, Stream& comm_serial, uint8_t * Byte4Array);
 
         void print4Bytes(Stream& debug_serial, Stream& comm_serial, uint8_t * Byte4Array);
 
-
     };
-    
-
-    
 }
-
 #endif //uart_comm_ovidius_h
 
 
